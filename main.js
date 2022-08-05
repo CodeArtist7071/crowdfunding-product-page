099b6026c9b411ff26d2b8a8903f03c31ee17acf
CodeArtist7071 <artistproysn@gmail.com>
2022-08-05 23:30:22 +0530

    main.js file created for last enhancement that is responsive menu

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-function toggle(){
-    var x = document.getElementById("right-navbar");
-    if(window.matchMedia("max-width:700px")){
-        if(x.style.display === "none"){
-            x.style.display = "block"
-        }else{
-            x.style.display = "none"
-        }
-    }
-   
+const navslide =()=>{
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav');
+    const btn = document.querySelectorAll('.select-reward');
+    const thanks = document.getElementById("thanks");
+    const got_btn = document.getElementById("got-it");
+    hamburger.addEventListener('click',()=>{
+      nav.classList.toggle("nav-active");
+      hamburger.classList.toggle('toggle');
+    });
+   btn.forEach((e,index)=>{
+    e.addEventListener('click',()=>{
+    thanks.style.display = "block"
+    });
+   });
+   got_btn.addEventListener('click',()=>{
+    thanks.style.display = "none"
+   })
+  }
+  navslide();
 
-}
